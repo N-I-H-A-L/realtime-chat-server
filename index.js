e183ab18418287f12136bde798be9e8fbdf5f321
N-I-H-A-L <notesnihalengg@gmail.com>
2023-07-08 22:09:58 +0530

    Origin Updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://realtime-chat-frontend-sigma.vercel.app/",
+        origin: "https://realtime-chat-frontend-sigma.vercel.app",
         methods: ["GET", "POST"],
     },
 });
